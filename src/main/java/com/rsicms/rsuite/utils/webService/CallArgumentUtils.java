a84c108745495f3b9afcae5f1e9cc92c78417495
Brent Hartwig <brent_d_hartwig@hotmail.com>
2016-06-29 13:32:22 -0400

    Added CallArgumentUtils.getArgumentsWithSameNamePrefix().  Reformatted this class and another.

--- src/main/java/com/rsicms/rsuite/utils/webService/CallArgumentUtils.java
+++ src/main/java/com/rsicms/rsuite/utils/webService/CallArgumentUtils.java
@@ -1,5 +1,9 @@
 package com.rsicms.rsuite.utils.webService;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 
 import com.reallysi.rsuite.api.remoteapi.CallArgument;
@@ -12,26 +16,52 @@ public class CallArgumentUtils {
    * 
    * @param args
    */
-  public static void logArguments(
-      CallArgumentList args,
-      Log log) {
+  public static void logArguments(CallArgumentList args, Log log) {
     StringBuilder sb = new StringBuilder("Parameters:");
     for (CallArgument arg : args.getAll()) {
-      sb.append(
-          "\n\t\"").append(
-          arg.getName()).append(
-          "\": ");
+      sb.append("\n\t\"").append(arg.getName()).append("\": ");
       if (arg.isFileItem()) {
         sb.append("[FileItem]");
       } else if (arg.isFile()) {
         sb.append("[File]");
       } else {
-        sb.append(
-            "\"").append(
-            arg.getValue()).append(
-            "\"");
+        sb.append("\"").append(arg.getValue()).append("\"");
       }
     }
     log.info(sb);
   }
+
+  /**
+   * Get call arguments with names that begin with the specified prefix
+   * 
+   * @param args Call argument list to inspect.
+   * @param prefix Prefix the call argument name must begin with, in order to qualify.
+   * @param stripPrefix Submit true if the prefix is to be stripped from the names of returned call
+   *        arguments.
+   * @param trimValue Submit true if whitespace is to be trimmed from the value of qualifying call
+   *        arguments.
+   * @param keepBlankStrings Submit true if args whose values are the empty string should be
+   *        retained
+   * @return List of call arguments that qualify.
+   */
+  protected static List<CallArgument> getArgumentsWithSameNamePrefix(CallArgumentList args,
+      String prefix, boolean stripPrefix, boolean trimValue, boolean keepBlankStrings) {
+    List<CallArgument> withPrefix = new ArrayList<CallArgument>();
+    if (args != null) {
+      for (CallArgument arg : args.getAll()) {
+        if (arg.getName().startsWith(prefix)
+            && (keepBlankStrings ? true : StringUtils.isNotBlank(arg.getValue()))) {
+          if (stripPrefix || trimValue) {
+            withPrefix.add(new CallArgument(
+                stripPrefix ? arg.getName().substring(prefix.length()) : arg.getName(),
+                trimValue ? arg.getValue().trim() : arg.getValue()));
+          } else {
+            withPrefix.add(arg);
+          }
+        }
+      }
+    }
+    return withPrefix;
+  }
+
 }
